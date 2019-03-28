07d8a5abb2ec48b2167d8772ffaa415e94dab471
ripper <453942056@qq.com>
2019-03-28 11:10:23 +0800

    no commit info

--- setup/ProjectManipulator.rb
+++ setup/ProjectManipulator.rb
@@ -50,8 +50,7 @@ module Pod
       classes_group = group.new_group('Classes', file_path + '/Classes')
       classes_group.new_reference('ReplaceMe.m')
       assets_group = group.new_group('Assets', file_path + '/Assets')
-      group.sort(:above)
-      group.sort(:above)
+      group.sort(1)
     end
 
     def remove_demo_project
