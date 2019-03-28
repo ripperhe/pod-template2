eef8286d3b2e4ae09af19472d77f6fa74b92c2a3
ripper <453942056@qq.com>
2019-03-28 15:23:21 +0800

    no commit info

--- setup/ProjectManipulator.rb
+++ setup/ProjectManipulator.rb
@@ -49,7 +49,7 @@ module Pod
       group = @project.main_group.new_group(@configurator.pod_name, file_path)
       classes_group = group.new_group('Classes', file_path + '/Classes')
       file_ref = classes_group.new_reference('ReplaceMe.m')
-      target = project.targets.first
+      target = @project.targets.first
       target.add_file_references([file_ref])
       assets_group = group.new_group('Assets', file_path + '/Assets')
     end
